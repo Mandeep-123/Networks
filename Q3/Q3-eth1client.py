2c60d7f4f3c5a10c7c736851478c31d57e5c7945
Mandeep-123 <38450179+Mandeep-123@users.noreply.github.com>
2021-10-14 22:59:04 +0530

    Added

---
+++ Q3/Q3-eth1client.py
@@ -0,0 +1,17 @@
+# Import socket module
+import socket
+
+# Create a socket object
+s = socket.socket()
+
+# Define the port on which you want to connect
+port = 8000
+
+# connect to the server on local computer
+s.connect(('192.168.1.5', port))
+msg ="comnetsii"
+s.send(msg.encode())
+# receive data from the server and decoding to get the string.
+print (s.recv(1024).decode())
+# close the connection
+s.close()
